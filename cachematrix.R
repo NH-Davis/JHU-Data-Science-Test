3ec6e95f280ae299fe05587ba43064d293a0485a
Nathaniel Hartley <nhartley1986@gmail.com>
2024-01-26 14:37:07 -0800

    Second submission of Week 3 Programming Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,35 @@
 ## The makeCacheMatrix function that sets and retrieves the value of the vector
-## and then sets and retrives the value of the mean for use in the subsequent
+## and then sets and retrives the value of the inverse for use in the subsequent
 ## function
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
+  inv <- NULL
   set <- function(y) {
     x <<- y
-    m <<- NULL
+    inv <<- NULL
   }
   get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
   list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
+
 
 
 ## The function below retrieves the vector created with the function above
 ## after checking to confirm if the mean has already been calculated - which
 ## reduces computational intensity
 cacheSolve <- function(x, ...) {
-  m <- x$getmean()
-  if(!is.null(m)) {
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
     message("getting cached data")
-    return(m)
+    return(inv)
   }
   data <- x$get()
-  m <- mean(data, ...)
-  x$setmean(m)
-  m
+  inv <- solve(data, ...)
+  x$setInverse(inv)
+  inv
 }
+
